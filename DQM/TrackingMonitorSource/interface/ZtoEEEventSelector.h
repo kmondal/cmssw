93df7ebf5daec8c41d3610922e5061c75be4b01b
kuntal <kuntal.mondal@cern.ch>
2019-03-11 20:01:28 +0100

    updated for 105X

--- DQM/TrackingMonitorSource/interface/ZtoEEEventSelector.h
+++ DQM/TrackingMonitorSource/interface/ZtoEEEventSelector.h
@@ -7,8 +7,7 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
-#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+
 
 class ZtoEEEventSelector : public edm::stream::EDFilter<> {
 public:
